4cdd5ed47167d99e15ff01577391fc4b3d8b1aa3
samiyah1 <umarsamiya@gmail.com>
2019-04-23 21:53:03 +0300

    adding static folders to the project

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mariam/Documents/python/pyprojects/django/alogotech/alogo/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
