9c6ca5549d682fb82a247288566caff9f8a53614
samiyah1 <umarsamiya@gmail.com>
2019-04-23 23:45:13 +0300

    configuring email

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.conf import settings
+from django.core.mail import send_mail
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alogo.settings")
@@ -20,3 +22,13 @@ if __name__ == "__main__":
             )
         raise
     execute_from_command_line(sys.argv)
+
+
+subject = 'Some subject'
+from_email = settings.DEFAULT_FROM_EMAIL
+message = 'This is my test message'
+recipient_list = ['dev@alogotech.com', 'umarsamiya@gmail.com']
+html_message = '<h1>This is my HTML test</h1>'
+
+
+send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=html_message)
